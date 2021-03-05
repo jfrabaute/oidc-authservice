5c9a9fadf4cb32a8e9c9585996918a6b7c889492
Adam Setters <9741640+asetty@users.noreply.github.com>
2021-03-05 13:24:55 -0500

    add test for ConfigAuthorizer

--- authorizer_groups_test.go
+++ authorizer_groups_test.go
@@ -52,3 +52,28 @@ func TestGroupsAuthorizer(t *testing.T) {
 		})
 	}
 }
+
+func TestLoadConfig(t *testing.T) {
+	input := []byte(`rules:
+  foo.bar.io:
+    groups:
+      - baz@bar.com
+      - beef@bar.com
+  theo.von.io:
+    groups:
+      - ratking@von.io
+      - plug@von.io`)
+
+	ca := &configAuthorizer{}
+	authzConfig, err := ca.parse(input)
+	if err != nil {
+		t.Errorf("error parsing config: %v", err)
+	}
+	t.Logf("loaded config: %v", *authzConfig)
+
+}
+
+func TestConfigAuthorizer(t *testing.T) {
+	ca := newConfigAuthorizer("./testdata/authz.yaml")
+	t.Logf("ca=%+v", ca)
+}
